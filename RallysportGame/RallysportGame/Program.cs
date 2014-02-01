881eb2d612ec73d18190310d1dd7db1a230e8d49
antonwestberg <antonwestberg_@hotmail.com>
2014-02-01 14:28:08 +0100

    Super basic, bara utgångspunkt

---
+++ RallysportGame/RallysportGame/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using OpenTK.Input;
+
+namespace RallysportGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new GameWindow())
+            {
+                game.Load += (sender, e) =>
+                {
+                    // setup settings, load textures, sounds
+                    game.VSync = VSyncMode.On;
+                };
+
+                game.Resize += (sender, e) =>
+                {
+                    GL.Viewport(0, 0, game.Width, game.Height);
+                };
+
+                game.UpdateFrame += (sender, e) =>
+                {
+                    // add game logic, input handling
+                    if (game.Keyboard[Key.Escape])
+                    {
+                        game.Exit();
+                    }
+                };
+
+                game.RenderFrame += (sender, e) =>
+                {
+                    // render graphics
+                    GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+                    GL.MatrixMode(MatrixMode.Projection);
+                    GL.LoadIdentity();
+                    GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+
+                    GL.Begin(PrimitiveType.Triangles);
+
+                    GL.Color3(Color.MidnightBlue);
+                    GL.Vertex2(-1.0f, 1.0f);
+                    GL.Color3(Color.SpringGreen);
+                    GL.Vertex2(0.0f, -1.0f);
+                    GL.Color3(Color.Ivory);
+                    GL.Vertex2(1.0f, 1.0f);
+
+                    GL.End();
+
+                    game.SwapBuffers();
+                };
+
+                // Run the game at 60 updates per second
+                game.Run(60.0);
+            }
+        }
+    }
+}
