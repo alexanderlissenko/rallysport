c180f1fc007bcb8c8af2cb7664685f02023f11bf
antonwestberg <antonwestberg_@hotmail.com>
2014-02-28 18:15:59 +0100

    Gravity working

--- RallysportGame/RallysportGame/CollisionHandler.cs
+++ RallysportGame/RallysportGame/CollisionHandler.cs
@@ -23,9 +23,6 @@ namespace RallysportGame
            // Box ground = new Box(Vector3.Zero, 30, 1, 30); //temporary invisible floor
             //space.Add(ground);
             //car = new Box(new Vector3(0, 4, 0), 1, 1, 1, 1);
-            foreach(DynamicEntity e in objects){
-                space.Add(e.GetBody());
-            }
             
             space.ForceUpdater.Gravity = new BEPUutilities.Vector3(0, -9.81f, 0);
         }
@@ -43,6 +40,7 @@ namespace RallysportGame
         public void addObject(DynamicEntity e)
         {
             objects.Add(e);
+            space.Add(e.GetBody());
         }
         public void setupEnvironment(Vector3[] vertices, int[] indices, Vector3 position) {
             //var mesh = new StaticMesh(vertices, indices, new AffineTransform(new Vector3(0, -40, 0)));
